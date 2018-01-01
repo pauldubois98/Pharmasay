e14a0213e50b6d75f3c3d260c69bfb0c77d34b3b
pauldubois98 <pauldubois98@gmail.com>
2018-01-01 23:54:45 +0100

    génération de la phrase pour le synthétiseur vocal à partir du formulaire

--- speech.js
+++ speech.js
@@ -1,6 +1,74 @@
-
-
-
 //responsiveVoice.speak("Bonjour", "French Female");
 
-var say = "Ce médicament est à prendre: ";
+
+
+var btDire = document.getElementById('dire');
+
+
+
+
+
+
+btDire.addEventListener("click", function (e) {
+    var nb = 0;
+    var tot = 0;
+    var say = "Ce médicament est à prendre: ";
+    
+    //counting
+    if (document.getElementById("matin").checked) {
+        tot = tot + 1;
+    }
+    if (document.getElementById("midi").checked) {
+        tot = tot + 1;
+    }
+    if (document.getElementById("soir").checked) {
+        tot = tot + 1;
+
+    }
+    //making the sentence
+    if (document.getElementById("matin").checked) {
+        //console.log("matin");
+        say = say + "matin, ";
+        nb = nb + 1;
+    }
+    if (document.getElementById("midi").checked) {
+        //console.log("midi");
+        nb = nb + 1;
+        if (nb === tot && tot > 1) {
+            say = say + 'et ';
+        }
+        say = say + "midi, ";
+    }
+    if (document.getElementById("soir").checked) {
+        //console.log("soir");
+        nb = nb + 1;
+        if (nb === tot && tot > 1) {
+            say = say + 'et ';
+        }
+        say = say + "soir, ";
+    }
+    
+    var voieEle = document.getElementsByName("voie");
+    for (var i = 0; i < voieEle.length; i++) {
+        if (voieEle[i].checked) {
+            say = say + "par voie " + voieEle[i].value
+        }
+        
+    }
+    
+    var position_repasEle = document.getElementsByName("position_repas");
+    for (var i = 0; i < position_repasEle.length; i++) {
+        if (position_repasEle[i].checked) {
+            say = say + ", " + position_repasEle[i].value
+        }
+        
+    }
+    
+    console.log(say);
+    responsiveVoice.speak(say, "French Female");
+
+});
+
+
+
+
